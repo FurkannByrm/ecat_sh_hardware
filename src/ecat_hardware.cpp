f19445da81edae56fe42a164449eb8510b79ad14
enaciodabasi <enaciodabasi@outlook.com>
2024-11-21 13:34:37 +0300

    Added EL1809 Digital Input and EL2809 Digital Output moduule to EtherCAT module.

--- src/ecat_hardware.cpp
+++ src/ecat_hardware.cpp
@@ -32,6 +32,30 @@ struct
   uint current_position;
   uint current_velocity;
 } RightMotorEthercatDataOffsets;
+
+struct IoChannelOffsets
+{
+  uint channel1;
+  uint channel2;
+  uint channel3;
+  uint channel4;
+  uint channel5;
+  uint channel6;
+  uint channel7;
+  uint channel8;
+  uint channel9;
+  uint channel10;
+  uint channel11;
+  uint channel12;
+  uint channel13;
+  uint channel14;
+  uint channel15;
+  uint channel16;
+};
+
+IoChannelOffsets digitalOutputOffsets;
+IoChannelOffsets digitalInputOffsets;
+
 /*
 const ec_pdo_entry_reg_t lifterDomainRegistries[] = {
     {SLAVE_START_POSITION, SLAVE_ALIAS, SLAVE_VENDOR_ID, SLAVE_PRODUCT_ID,
@@ -49,6 +73,98 @@ const ec_pdo_entry_reg_t lifterDomainRegistries[] = {
     {SLAVE_START_POSITION, SLAVE_ALIAS, SLAVE_VENDOR_ID, SLAVE_PRODUCT_ID,
      0x606C, 0x0, &EthercatDataOffsets.current_velocity},
     {}};*/
+
+ec_pdo_entry_info_t right_motor_pdo_entries[] = { { 0x6040, 0x00, 16 }, { 0x6060, 0x00, 8 },  { 0x607a, 0x00, 32 },
+                                                  { 0x60ff, 0x00, 32 }, { 0x6041, 0x00, 16 }, { 0x6064, 0x00, 32 },
+                                                  { 0x606c, 0x00, 32 } };
+
+ec_pdo_info_t right_motor_pdo_info[] = { { 0x1603, 4, right_motor_pdo_entries + 0 },
+                                         { 0x1A03, 3, right_motor_pdo_entries + 4 } };
+
+ec_sync_info_t right_motor_slave_syncs[] = { { 0, EC_DIR_OUTPUT, 0, NULL, EC_WD_DISABLE },
+                                             { 1, EC_DIR_INPUT, 0, NULL, EC_WD_DISABLE },
+                                             { 2, EC_DIR_OUTPUT, 1, right_motor_pdo_info + 0, EC_WD_ENABLE },
+                                             { 3, EC_DIR_INPUT, 1, right_motor_pdo_info + 1, EC_WD_DISABLE },
+                                             { 0xff } };
+
+ec_pdo_entry_info_t left_motor_pdo_entries[] = { { 0x6040, 0x00, 16 }, { 0x6060, 0x00, 8 },  { 0x607a, 0x00, 32 },
+                                                 { 0x60ff, 0x00, 32 }, { 0x6041, 0x00, 16 }, { 0x6064, 0x00, 32 },
+                                                 { 0x606c, 0x00, 32 } };
+
+ec_pdo_info_t left_motor_pdo_info[] = { { 0x1603, 4, left_motor_pdo_entries + 0 },
+                                        { 0x1A03, 3, left_motor_pdo_entries + 4 } };
+
+ec_sync_info_t left_motor_slave_syncs[] = { { 0, EC_DIR_OUTPUT, 0, NULL, EC_WD_DISABLE },
+                                            { 1, EC_DIR_INPUT, 0, NULL, EC_WD_DISABLE },
+                                            { 2, EC_DIR_OUTPUT, 1, left_motor_pdo_info + 0, EC_WD_ENABLE },
+                                            { 3, EC_DIR_INPUT, 1, left_motor_pdo_info + 1, EC_WD_DISABLE },
+                                            { 0xff } };
+
+constexpr auto DIGITAL_INPUT_START_POSITION = 3;
+constexpr auto DIGITAL_INPUT_ALIAS = 0;
+constexpr auto DIGITAL_INPUT_VENDOR_ID = 0x00000002;
+constexpr auto DIGITAL_INPUT_PRODUCT_ID = 0x07113052;
+ec_pdo_entry_info_t digital_input_pdo_entries[] = {
+  { 0x6000, 0x01, 1 }, { 0x6010, 0x01, 1 }, { 0x6020, 0x01, 1 }, { 0x6030, 0x01, 1 },
+  { 0x6040, 0x01, 1 }, { 0x6050, 0x01, 1 }, { 0x6060, 0x01, 1 }, { 0x6070, 0x01, 1 },
+  { 0x6080, 0x01, 1 }, { 0x6090, 0x01, 1 }, { 0x60a0, 0x01, 1 }, { 0x60b0, 0x01, 1 },
+  { 0x60c0, 0x01, 1 }, { 0x60d0, 0x01, 1 }, { 0x60e0, 0x01, 1 }, { 0x60f0, 0x01, 1 },
+};
+
+ec_pdo_info_t digital_input_pdos[] = {
+  { 0x1a00, 1, digital_input_pdo_entries + 0 },  /* Channel 1 */
+  { 0x1a01, 1, digital_input_pdo_entries + 1 },  /* Channel 2 */
+  { 0x1a02, 1, digital_input_pdo_entries + 2 },  /* Channel 3 */
+  { 0x1a03, 1, digital_input_pdo_entries + 3 },  /* Channel 4 */
+  { 0x1a04, 1, digital_input_pdo_entries + 4 },  /* Channel 5 */
+  { 0x1a05, 1, digital_input_pdo_entries + 5 },  /* Channel 6 */
+  { 0x1a06, 1, digital_input_pdo_entries + 6 },  /* Channel 7 */
+  { 0x1a07, 1, digital_input_pdo_entries + 7 },  /* Channel 8 */
+  { 0x1a08, 1, digital_input_pdo_entries + 8 },  /* Channel 9 */
+  { 0x1a09, 1, digital_input_pdo_entries + 9 },  /* Channel 10 */
+  { 0x1a0a, 1, digital_input_pdo_entries + 10 }, /* Channel 11 */
+  { 0x1a0b, 1, digital_input_pdo_entries + 11 }, /* Channel 12 */
+  { 0x1a0c, 1, digital_input_pdo_entries + 12 }, /* Channel 13 */
+  { 0x1a0d, 1, digital_input_pdo_entries + 13 }, /* Channel 14 */
+  { 0x1a0e, 1, digital_input_pdo_entries + 14 }, /* Channel 15 */
+  { 0x1a0f, 1, digital_input_pdo_entries + 15 }, /* Channel 16 */
+};
+
+ec_sync_info_t digital_input_syncs[] = { { 0, EC_DIR_INPUT, 16, digital_input_pdos + 0, EC_WD_DISABLE }, { 0xff } };
+
+constexpr auto DIGITAL_OUTPUT_START_POSITION = 4;
+constexpr auto DIGITAL_OUTPUT_ALIAS = 0;
+constexpr auto DIGITAL_OUTPUT_VENDOR_ID = 0x00000002;
+constexpr auto DIGITAL_OUTPUT_PRODUCT_ID = 0x0af93052;
+ec_pdo_entry_info_t digital_output_pdo_entries[] = {
+  { 0x7000, 0x01, 1 }, { 0x7010, 0x01, 1 }, { 0x7020, 0x01, 1 }, { 0x7030, 0x01, 1 },
+  { 0x7040, 0x01, 1 }, { 0x7050, 0x01, 1 }, { 0x7060, 0x01, 1 }, { 0x7070, 0x01, 1 },
+  { 0x7080, 0x01, 1 }, { 0x7090, 0x01, 1 }, { 0x70a0, 0x01, 1 }, { 0x70b0, 0x01, 1 },
+  { 0x70c0, 0x01, 1 }, { 0x70d0, 0x01, 1 }, { 0x70e0, 0x01, 1 }, { 0x70f0, 0x01, 1 },
+
+};
+
+ec_pdo_info_t digital_output_pdos[] = {
+  { 0x1600, 1, digital_output_pdo_entries + 0 },  /* Channel 1 */
+  { 0x1601, 1, digital_output_pdo_entries + 1 },  /* Channel 2 */
+  { 0x1602, 1, digital_output_pdo_entries + 2 },  /* Channel 3 */
+  { 0x1603, 1, digital_output_pdo_entries + 3 },  /* Channel 4 */
+  { 0x1604, 1, digital_output_pdo_entries + 4 },  /* Channel 5 */
+  { 0x1605, 1, digital_output_pdo_entries + 5 },  /* Channel 6 */
+  { 0x1606, 1, digital_output_pdo_entries + 6 },  /* Channel 7 */
+  { 0x1607, 1, digital_output_pdo_entries + 7 },  /* Channel 8 */
+  { 0x1608, 1, digital_output_pdo_entries + 8 },  /* Channel 9 */
+  { 0x1609, 1, digital_output_pdo_entries + 9 },  /* Channel 10 */
+  { 0x160a, 1, digital_output_pdo_entries + 10 }, /* Channel 11 */
+  { 0x160b, 1, digital_output_pdo_entries + 11 }, /* Channel 12 */
+  { 0x160c, 1, digital_output_pdo_entries + 12 }, /* Channel 13 */
+  { 0x160d, 1, digital_output_pdo_entries + 13 }, /* Channel 14 */
+  { 0x160e, 1, digital_output_pdo_entries + 14 }, /* Channel 15 */
+  { 0x160f, 1, digital_output_pdo_entries + 15 }, /* Channel 16 */
+
+};
+
+ec_sync_info_t digital_output_syncs[] = { { 0, EC_DIR_INPUT, 16, digital_output_pdos + 0, EC_WD_DISABLE }, { 0xff } };
 
 const ec_pdo_entry_reg_t domainRegistries[] = {
   { SLAVE_ALIAS, SLAVE_START_POSITION, SLAVE_VENDOR_ID, SLAVE_PRODUCT_ID, 0x6040, 0x00,
@@ -79,34 +195,72 @@ const ec_pdo_entry_reg_t domainRegistries[] = {
     &LeftMotorEthercatDataOffsets.current_position },
   { SLAVE_ALIAS, SLAVE_START_POSITION + 1, SLAVE_VENDOR_ID, SLAVE_PRODUCT_ID, 0x606C, 0x00,
     &LeftMotorEthercatDataOffsets.current_velocity },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6000, 0x01,
+    &digitalInputOffsets.channel1 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6010, 0x01,
+    &digitalInputOffsets.channel2 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6020, 0x01,
+    &digitalInputOffsets.channel3 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6030, 0x01,
+    &digitalInputOffsets.channel4 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6040, 0x01,
+    &digitalInputOffsets.channel5 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6050, 0x01,
+    &digitalInputOffsets.channel6 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6060, 0x01,
+    &digitalInputOffsets.channel7 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6070, 0x01,
+    &digitalInputOffsets.channel8 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6080, 0x01,
+    &digitalInputOffsets.channel9 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x6090, 0x01,
+    &digitalInputOffsets.channel10 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x60a0, 0x01,
+    &digitalInputOffsets.channel11 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x60b0, 0x01,
+    &digitalInputOffsets.channel12 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x60c0, 0x01,
+    &digitalInputOffsets.channel13 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x60d0, 0x01,
+    &digitalInputOffsets.channel14 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x60e0, 0x01,
+    &digitalInputOffsets.channel15 },
+  { DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION, DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID, 0x60f0, 0x01,
+    &digitalInputOffsets.channel16 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7000,
+    0x01, &digitalOutputOffsets.channel1 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7010,
+    0x01, &digitalOutputOffsets.channel2 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7020,
+    0x01, &digitalOutputOffsets.channel3 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7030,
+    0x01, &digitalOutputOffsets.channel4 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7040,
+    0x01, &digitalOutputOffsets.channel5 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7050,
+    0x01, &digitalOutputOffsets.channel6 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7060,
+    0x01, &digitalOutputOffsets.channel7 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7070,
+    0x01, &digitalOutputOffsets.channel8 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7080,
+    0x01, &digitalOutputOffsets.channel9 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x7090,
+    0x01, &digitalOutputOffsets.channel10 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x70a0,
+    0x01, &digitalOutputOffsets.channel11 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x70b0,
+    0x01, &digitalOutputOffsets.channel12 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x70c0,
+    0x01, &digitalOutputOffsets.channel13 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x70d0,
+    0x01, &digitalOutputOffsets.channel14 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x70e0,
+    0x01, &digitalOutputOffsets.channel15 },
+  { DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION, DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID, 0x70f0,
+    0x01, &digitalOutputOffsets.channel16 },
   {}
 };
-
-ec_pdo_entry_info_t right_motor_pdo_entries[] = { { 0x6040, 0x00, 16 }, { 0x6060, 0x00, 8 },  { 0x607a, 0x00, 32 },
-                                                  { 0x60ff, 0x00, 32 }, { 0x6041, 0x00, 16 }, { 0x6064, 0x00, 32 },
-                                                  { 0x606c, 0x00, 32 } };
-
-ec_pdo_info_t right_motor_pdo_info[] = { { 0x1603, 4, right_motor_pdo_entries + 0 },
-                                         { 0x1A03, 3, right_motor_pdo_entries + 4 } };
-
-ec_sync_info_t right_motor_slave_syncs[] = { { 0, EC_DIR_OUTPUT, 0, NULL, EC_WD_DISABLE },
-                                             { 1, EC_DIR_INPUT, 0, NULL, EC_WD_DISABLE },
-                                             { 2, EC_DIR_OUTPUT, 1, right_motor_pdo_info + 0, EC_WD_ENABLE },
-                                             { 3, EC_DIR_INPUT, 1, right_motor_pdo_info + 1, EC_WD_DISABLE },
-                                             { 0xff } };
-
-ec_pdo_entry_info_t left_motor_pdo_entries[] = { { 0x6040, 0x00, 16 }, { 0x6060, 0x00, 8 },  { 0x607a, 0x00, 32 },
-                                                 { 0x60ff, 0x00, 32 }, { 0x6041, 0x00, 16 }, { 0x6064, 0x00, 32 },
-                                                 { 0x606c, 0x00, 32 } };
-
-ec_pdo_info_t left_motor_pdo_info[] = { { 0x1603, 4, left_motor_pdo_entries + 0 },
-                                        { 0x1A03, 3, left_motor_pdo_entries + 4 } };
-
-ec_sync_info_t left_motor_slave_syncs[] = { { 0, EC_DIR_OUTPUT, 0, NULL, EC_WD_DISABLE },
-                                            { 1, EC_DIR_INPUT, 0, NULL, EC_WD_DISABLE },
-                                            { 2, EC_DIR_OUTPUT, 1, left_motor_pdo_info + 0, EC_WD_ENABLE },
-                                            { 3, EC_DIR_INPUT, 1, left_motor_pdo_info + 1, EC_WD_DISABLE },
-                                            { 0xff } };
 
 bool runHardwareLoop = true;
 
@@ -162,6 +316,26 @@ int main(int argc, char** argv)
     return 1;
   }
 
+  ecrt_slave_config_dc(slaveConfigPtr, 0x0300, 2000000, 1000000, 2000000, 0);
+
+  slaveConfigPtr = ecrt_master_slave_config(masterPtr, DIGITAL_INPUT_ALIAS, DIGITAL_INPUT_START_POSITION,
+                                            DIGITAL_INPUT_VENDOR_ID, DIGITAL_INPUT_PRODUCT_ID);
+  configurePdosRes = ecrt_slave_config_pdos(slaveConfigPtr, EC_END, digital_input_syncs);
+  if (configurePdosRes != 0)
+  {
+    std::cout << "Could not configure digital input PDOs." << std::endl;
+    return 1;
+  }
+
+    slaveConfigPtr = ecrt_master_slave_config(masterPtr, DIGITAL_OUTPUT_ALIAS, DIGITAL_OUTPUT_START_POSITION,
+                                            DIGITAL_OUTPUT_VENDOR_ID, DIGITAL_OUTPUT_PRODUCT_ID);
+  configurePdosRes = ecrt_slave_config_pdos(slaveConfigPtr, EC_END, digital_output_syncs);
+  if (configurePdosRes != 0)
+  {
+    std::cout << "Could not configure digital input PDOs." << std::endl;
+    return 1;
+  }
+
   int registerDomainEntriesRes = ecrt_domain_reg_pdo_entry_list(domainPtr, domainRegistries);
 
   if (registerDomainEntriesRes != 0)
@@ -170,7 +344,7 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  ecrt_slave_config_dc(slaveConfigPtr, 0x0300, 2000000, 1000000, 2000000, 0);
+  
 
   // Set current thread scheduler and priority:
 
